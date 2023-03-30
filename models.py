4fc469ac27316f7af571f3f2c1c0639a70320c2b
Brian Tang <ashrielbrian@gmail.com>
2023-03-31 01:37:35 +0800

    Adds data models

---
+++ models.py
@@ -0,0 +1,31 @@
+import typing
+from dataclasses import dataclass
+
+
+@dataclass
+class Segment:
+    id: int
+
+    # time in seconds
+    start: float
+    end: float
+
+    text: str
+    tokens: typing.List[int]
+    emb: typing.Optional[typing.List[float]] = None
+
+
+@dataclass
+class Video:
+    video_id: str
+    title: str
+
+    transcription: str
+    segments: typing.List[Segment] = None
+    playlist_id: typing.Optional[str] = None
+    description: str = None
+    url: str = None
+
+    # source files
+    audio_file: typing.Optional[str] = None
+    transcription_file: typing.Optional[str] = None
