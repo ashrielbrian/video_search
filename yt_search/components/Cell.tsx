e23dabe0945e8038602b5fd5365e5139c52e9d67
Brian Tang <ashrielbrian@gmail.com>
2023-05-06 02:31:35 +0800

    Redesigns cell and grid layout

--- yt_search/components/Cell.tsx
+++ yt_search/components/Cell.tsx
@@ -6,7 +6,7 @@ export const Cell = ({ video }: { video: Video | VideoSearch }) => {
 
 
     let url = `https://youtube.com/watch?v=${video.id}`;
-    let desc = video.description?.substring(0, 250) + "...";
+    let desc = video.description?.substring(0, 500) + "...";
 
     if (video.start_time) {
         url = url + `&t=${Math.floor(video.start_time)}`
@@ -17,21 +17,36 @@ export const Cell = ({ video }: { video: Video | VideoSearch }) => {
     }
 
     return (
-        <div className="bg-white rounded-lg shadow-lg p-4 mt-2 transition-transform duration-500 transform hover:-translate-y-1 hover:scale-105">
-            <div className="flex flex-row">
 
-                {video.thumbnail && <Image src={video.thumbnail} alt="Placeholder image" className="w-full h-48 object-cover rounded-t-lg" height={150} width={150} />}
 
-                <div className="p-4">
+        <div className="bg-white rounded-lg shadow-lg p-4 mt-2 transition-transform duration-500 transform hover:-translate-y-1 hover:scale-102 flex flex-col min-h-250">
+            <div className="flex flex-row flex-grow">
+
+                {video.thumbnail && <Image src={video.thumbnail} alt="Placeholder image" className="w-full h-48 object-cover rounded-t-lg" height={100} width={100} />}
+
+                <div className="flex flex-col p-4">
                     <Link href={url} target="_blank">
-                        <h2 className="text-lg font-bold mb-2">{video.title}</h2>
+                        <h2 className="text-lg font-bold mb-2 text-grey-500 hover:text-indigo-700 hover:underline transition-colors">{video.title}</h2>
                     </Link>
-                    <p className="text-gray-700">{desc}</p>
-                    <Link href={`/summary/${video.id}`}>
-                        <button className="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 hover:bg-blue-600 ransition-colors duration-300 ease-in-out">View Summary</button>
-                    </Link>
+
+                    <p className="text-gray-700 flex-grow">{desc}</p>
+
+                    <div className="flex flex-row justify-center">
+                        {video.start_time ? (
+                            <Link href={url} target="_blank">
+                                <button className="bg-transparent text-indigo-500 font-semibold border border-indigo-500 py-2 px-4 mt-4 mr-2 hover:bg-indigo-600  hover:text-white transition-colors duration-300 ease-in-out">Listen to Snippet</button>
+                            </Link>
+                        ) : (
+                            <Link href={url} target="_blank">
+                                <button className="bg-indigo-500 text-white py-2 px-4 mt-4 hover:bg-indigo-600 transition-colors duration-300 ease-in-out">Watch Sermon</button>
+                            </Link>
+                        )}
+                    </div>
+
                 </div>
             </div>
-        </div >
+
+        </div>
+
     )
 }
