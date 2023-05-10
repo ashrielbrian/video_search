d8c7446fe66a839675ab029a2ae99fcb03dce1e8
Brian Tang <ashrielbrian@gmail.com>
2023-05-11 03:04:48 +0800

    Beautifies layout; updates css

--- yt_search/app/layout.tsx
+++ yt_search/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={assistant.className}>
+            <body className={assistant.className + " bg-stone-50"}>
 
                 <Header />
 
