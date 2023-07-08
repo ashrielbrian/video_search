7bf830d64d0f9905b68159bd8adc18c6c27cd97d
Brian Tang <ashrielbrian@gmail.com>
2023-07-08 22:34:51 +0800

    Fixes loading spinner position

--- yt_search/app/loading.tsx
+++ yt_search/app/loading.tsx
@@ -5,7 +5,9 @@ import { CircularProgress } from "@chakra-ui/react";
 const Loading = () => {
     return (
         // Change the size to 120px
-        <CircularProgress value={30} size="120px" />
+        <div className="max-w-7xl mx-auto pt-4 text-center">
+            <CircularProgress isIndeterminate={true} color="indigo.500" />
+        </div>
     );
 };
 export default Loading;
