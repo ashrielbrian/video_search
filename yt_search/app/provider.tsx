f58ecd8f888fea4d465fb8b8ed6bad7e5b9d3b7a
Brian Tang <ashrielbrian@gmail.com>
2024-12-10 22:39:43 +0800

    Removes unnecessary CacheProvider from chakra nextjs

--- yt_search/app/provider.tsx
+++ yt_search/app/provider.tsx
@@ -1,19 +1,16 @@
 // app/providers.tsx
 'use client'
 
-import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
 
-export function Providers({ 
-    children 
-  }: { 
-  children: React.ReactNode 
-  }) {
-  return (
-    <CacheProvider>
-      <ChakraProvider>
-        {children}
-      </ChakraProvider>
-    </CacheProvider>
-  )
+export function Providers({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <ChakraProvider>
+            {children}
+        </ChakraProvider>
+    )
 }
