da4c351f9053533193892709c68008e6347c07c6
Brian Tang <briant@Brians-MacBook-Pro.local>
2024-12-10 22:23:21 +0800

    Extends Video type to include a unique key

--- yt_search/lib/types.ts
+++ yt_search/lib/types.ts
@@ -1,16 +1,18 @@
-import { Database } from "./database.types"
+import { Database } from "./database.types";
 
-export type Video = Database['public']['Tables']['video']['Row'];
-export type Segment = Database['public']['Tables']['segment']['Row'];
+type BaseVideo = Database["public"]["Tables"]["video"]["Row"];
+export type Segment = Database["public"]["Tables"]["segment"]["Row"];
+
+export type Video = BaseVideo & {
+    key?: string;
+};
 
 type VideoSegment = {
-    segment_id: number
-    content: string
-    similarity: number
-    start_time: number
-    end_time: number
-}
+    segment_id: number;
+    content: string;
+    similarity: number;
+    start_time: number;
+    end_time: number;
+};
 
 export type VideoSearch = Video & VideoSegment;
-
-
