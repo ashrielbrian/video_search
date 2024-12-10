58bc4ee92ecc077b0aba06ca3591e95fc07e296f
Brian Tang <briant@Brians-MacBook-Pro.local>
2024-12-10 22:24:13 +0800

    Minor updates and fixes

--- yt_search/app/api/embedding/route.ts
+++ yt_search/app/api/embedding/route.ts
@@ -1,39 +1,44 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 import supabase from "@/utils/supabase";
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
 const getEmbedding = async (query: string) => {
-    const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
     const openai = new OpenAIApi(configuration);
 
+    console.log("Waiting for OpenAI...");
     const embeddingResp = await openai.createEmbedding({
         model: "text-embedding-ada-002",
-        input: query
+        input: query,
     });
+    console.log("Embeddings returned.");
 
     const [{ embedding }] = embeddingResp.data.data;
     const bestResults = await search(embedding);
 
     return bestResults;
-}
+};
 
 const search = async (queryEmbedding: number[]) => {
     const results = await supabase.rpc("search_segment", {
         query_embedding: JSON.stringify(queryEmbedding),
         similarity_threshold: 0.78,
-        match_count: 10
+        match_count: 10,
     });
 
-    console.log(results)
-    return results;
-}
-
+    return results.data?.map((item) => ({
+        ...item,
+        key: item.id + "_" + item.segment_id,
+    }));
+};
 
 export async function POST(request: Request) {
-    const { query } = await request.json()
+    const { query } = await request.json();
 
     if (query) {
         const results = await getEmbedding(query);
-        return NextResponse.json({ results })
+        return NextResponse.json({ results });
     }
 }
--- yt_search/components/Grid.tsx
+++ yt_search/components/Grid.tsx
@@ -5,7 +5,7 @@ export const Grid = ({ videos }: { videos: Video[] }) => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6 mt-6">
             {videos.map((video) => (
-                <Cell key={video.id} video={video} />
+                <Cell key={video.key || video.id} video={video} />
             ))}
         </div>
     );
