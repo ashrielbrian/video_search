7ec85449f4d9efea6b7801070f5352ad1c8bad7a
Brian Tang <briant@Brians-MacBook-Pro.local>
2024-12-10 22:23:01 +0800

    Removes unneeded experimental nextjs appDir

--- yt_search/next.config.js
+++ yt_search/next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
-    images: {
-        remotePatterns: [{
-            protocol: "https",
-            hostname: "i.ytimg.com",
-            port: "",
-        }]
-    }
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
